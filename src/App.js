704e520b7f5f7f7dacda462cc08cd08988bc3a1c
AskerovRoman <bythesword2@yandex.ru>
2024-03-19 16:09:33 +0300

    Components first homework

--- src/App.js
+++ src/App.js
@@ -1,39 +1,66 @@
-// декларативный
-import { createElement } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
 
-// императивный
 export const App = () => {
-	const year = new Date().getFullYear();
+	const [value, setValue] = useState('');
+	const [list, setList] = useState([]);
+	const [error, setError] = useState('');
+
+	const validate = (val) => val.length >= 3;
+
+	const onInputButtonClick = () => {
+		const promptValue = prompt('Введите значение');
+		if (validate(promptValue)) {
+			setValue(promptValue);
+			setError('');
+		} else {
+			setError('Введенное значение должно содержать минимум 3 символа');
+		}
+	};
+
+	const isValueValid = validate(value);
+
+	const onAddButtonClick = () => {
+		if (!isValueValid) return;
+		setList((prev) => [...prev, { id: Date.now(), value }]);
+		setValue('');
+		setError('');
+	};
+
 	return (
-		// декларативный
-		createElement(
-			'div',
-			{ className: 'App' },
-			createElement(
-				'header',
-				{ className: 'App-header' },
-				createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-				createElement(
-					'p',
-					null,
-					'Edit ',
-					createElement('code', null, 'src/App.js'),
-					' and save to reload2.',
-				),
-				createElement(
-					'a',
-					{
-						className: 'App-link',
-						href: 'https://reactjs.org',
-						target: '_blank',
-						rel: 'noopener noreferrer',
-					},
-					'Learn React',
-				),
-				createElement('div', null, year),
-			),
-		)
+		<div className={styles.app}>
+			<h1 className={styles['page-heading']}>Ввод значения</h1>
+			<p className={styles['no-margin-text']}>
+				Текущее значение <code>value</code>: "
+				<output className={styles['current-value']}>{value}</output>"
+			</p>
+			{error !== '' && <div className={styles.error}>{error}</div>}
+			<div className={styles['buttons-container']}>
+				<button className={styles.button} onClick={onInputButtonClick}>
+					Ввести новое
+				</button>
+				<button
+					className={styles.button}
+					onClick={onAddButtonClick}
+					disabled={!isValueValid}
+				>
+					Добавить в список
+				</button>
+			</div>
+			<div className={styles['list-container']}>
+				<h2 className={styles['list-heading']}>Список:</h2>
+				{list.length > 0 ? (
+					<ul className={styles.list}>
+						{list.map((item) => (
+							<li className={styles['list-item']} key={item.id}>
+								{item.value}
+							</li>
+						))}
+					</ul>
+				) : (
+					<p className={styles['no-margin-text']}>Нет добавленных элементов</p>
+				)}
+			</div>
+		</div>
 	);
 };
